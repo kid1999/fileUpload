9460b8e69787951c5a721e8455381f98ea566a31
kid1999 <kid.1447250889@live.com>
2019-12-22 14:33:29 +0800

    完成打包下载功能
    +使用 xhr发送ajax + blob格式接收数据

--- src/main/java/kid1999/upload/controller/download.java
+++ src/main/java/kid1999/upload/controller/download.java
@@ -1,6 +1,5 @@
 package kid1999.upload.controller;
 
-import kid1999.upload.dto.Result;
 import kid1999.upload.dto.ZipModel;
 import kid1999.upload.model.Student;
 import kid1999.upload.utils.ZipUtil;
@@ -50,39 +49,12 @@ public class download {
 				zipUtil.zipFile(zipModelList, toClient);
 				toClient.close();
 				response.setHeader("content-disposition", "attachment;fileName=" + fileName);
+				response.setContentType("blob");
 				//todo:将zip文件下载下来
 				zipUtil.downloadZip(file, response);
 			}catch (Exception e){
 				log.error(e.getMessage());
 			}
 		}
-//		try {
-//			if (request.getParameterValues("filenames") == null) {
-//				response.sendRedirect(request.getHeader("REFERER"));
-//			} else {
-//				List<ZipModel> zipModelList = new ArrayList<>();
-//				String[] filenames = request.getParameterValues("filenames");
-//				for (int i = 0; i < filenames.length; i++) {
-//					String[] files = filenames[i].split(" ");
-//					zipModelList.add(new ZipModel(files[0], files[1]));
-//				}
-//				//todo:设置打包后的文件名
-//				String fileName = "File.zip";
-//				//todo:临时文件目录,用于存储打包的下载文件
-//				String globalUploadPath = request.getSession().getServletContext().getRealPath("/");
-//				String outFilePath = globalUploadPath + File.separator + fileName;
-//				File file = new File(outFilePath);
-//				//文件输出流 压缩流
-//				ZipOutputStream toClient = new ZipOutputStream(new FileOutputStream(file));
-//				//todo:调用通用方法下载fastfds文件，打包成zip文件
-//				zipUtil.zipFile(zipModelList, toClient);
-//				toClient.close();
-//				response.setHeader("content-disposition", "attachment;fileName=" + fileName);
-//				//todo:将zip文件下载下来
-//				zipUtil.downloadZip(file, response);
-//			}
-//		} catch (Exception e) {
-//			log.error(e.getMessage());
-//		}
 	}
 }
